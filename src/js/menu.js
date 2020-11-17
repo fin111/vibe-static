532fa27dd288c09b8125e1b00e250ae31139a8b6
Nathan Croawell <nathancroawell@gmail.com>
2020-11-17 20:43:32 +0000

    added keyboard accessibility and style to menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,10 +1,30 @@
 // This will get imported in main.js
 export const burgerMenu = () => {
-    const menuBtn = document.querySelector(".menu");
-    const navigation = document.querySelector(".navigation");
+    const menuBtn = document.querySelector(".menu-container"),
+    navigation = document.querySelector(".navigation"),
+    navigationListItem = document.querySelectorAll(".navigation li"),
+    key = {escape: 27};
 
+    // Toggles the menu open and close
     menuBtn.addEventListener("click", function(){
         menuBtn.classList.toggle("open");
         navigation.classList.toggle("open");
     });
+
+    //Close menu when one of the li's is clicked
+    for (let item of navigationListItem) {
+        item.addEventListener("click", function(){
+            menuBtn.classList.remove("open");
+            navigation.classList.remove("open");
+        });
+    }
+
+    // add the key listener to close the menu when hitting esc
+    window.addEventListener('keydown', HandleEscKeyEvent);
+    function HandleEscKeyEvent (e){
+        if(e.keyCode === key.escape) {
+            menuBtn.classList.remove("open");
+            navigation.classList.remove("open");
+        }
+    }
 };
